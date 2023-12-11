88b0a3c44c1e1ff87db8c249d86fa0a536c9a084
Logan McDonald <lmcdonald@precisiondrilling.com>
2023-12-11 10:36:13 -0700

    Completed day 4

---
+++ day4/part1.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+day4Dir = os.path.dirname(__file__)
+sampleFilename = os.path.join(day4Dir, 'sampleInput1.txt')
+inputFilename = os.path.join(day4Dir, 'input.txt')
+
+def calculateCardPoints(filename):
+    result = 0
+    with open(filename) as f:
+        for line in f.readlines():
+            points = 0
+            line = line.split(':')[1].split('|')
+            winning_nums = re.findall(r'\d+', line[0])
+            my_nums = re.findall(r'\d+', line[1])
+            for num in my_nums:
+                if num in winning_nums:
+                    if points == 0:
+                        points = 1
+                    else: 
+                        points *= 2
+            result += points
+    print('Answer for {} is {}'.format(filename, result))
+
+
+calculateCardPoints(sampleFilename)
+calculateCardPoints(inputFilename)
+
+
---
+++ day4/part2.py
@@ -0,0 +1,49 @@
+import os
+import re
+
+day4Dir = os.path.dirname(__file__)
+sampleFilename = os.path.join(day4Dir, 'sampleInput1.txt')
+inputFilename = os.path.join(day4Dir, 'input.txt')
+
+def getNumScratchcards(i, lines, memo):
+    if i in memo: 
+        return memo[i]
+    
+    nums = lines[i].split(':')[1].split('|')
+    pattern = re.compile(r'\d+')
+    winning_nums = pattern.findall(nums[0])
+    my_nums = pattern.findall(nums[1])
+
+    num_cards = 1
+    matching_nums = 0
+    for num in my_nums:
+        if num in winning_nums:
+            matching_nums += 1
+            num_cards += getNumScratchcards(i + matching_nums, lines, memo)
+    memo[i] = num_cards
+    return memo[i]
+
+def getTotalScratchCards(filename):
+    memo = {}
+    with open(filename) as f:
+        lines = f.readlines()
+        result = 0
+        for i in range(len(lines)):
+            result += getNumScratchcards(i, lines, memo)
+            # points = 0
+            # line = line.split(':')[1].split('|')
+            # winning_nums = re.findall(r'\d+', line[0])
+            # my_nums = re.findall(r'\d+', line[1])
+            # for num in my_nums:
+            #     if num in winning_nums:
+            #         if points == 0:
+            #             points = 1
+            #         else: 
+            #             points *= 2
+            # result += points
+    print('Answer for {} is {}'.format(filename, result))
+
+getTotalScratchCards(sampleFilename)
+getTotalScratchCards(inputFilename)
+
+
